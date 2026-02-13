813c7c617025c69fe25f8a6d1545ebd59eae7a17
LucaMezz <lucamezza4@gmail.com>
2026-02-13 23:27:37 +1100

    feat: added more keywords and operators and more parsing

--- grammar.js
+++ grammar.js
@@ -48,9 +48,36 @@ export default grammar({
       $.newline
     ),
 
-    parameters: $ => sep1($.identifier, ","),
+    parameters: $ => sep1($.parameter, ","),
 
-    type_annotation: $ => $.identifier,
+    parameter: $ => seq($.identifier, ":", $.type_annotation),
+
+    type_annotation: $ => choice(
+      $.type_u8,
+      $.type_u16,
+      $.type_u32,
+      $.type_u64,
+      $.type_i8,
+      $.type_i16,
+      $.type_i32,
+      $.type_i64,
+      $.type_f32,
+      $.type_f64,
+      $.type_bool,
+      $.identifier
+    ),
+
+    type_u8: $ => token("u8"),
+    type_u16: $ => token("u16"),
+    type_u32: $ => token("u32"),
+    type_u64: $ => token("u64"),
+    type_i8: $ => token("i8"),
+    type_i16: $ => token("i16"),
+    type_i32: $ => token("i32"),
+    type_i64: $ => token("i64"),
+    type_f32: $ => token("f32"),
+    type_f64: $ => token("f64"),
+    type_bool: $ => token("bool"),
 
     expression: $ => choice(
       $.number,
